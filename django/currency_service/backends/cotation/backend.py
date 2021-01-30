362318a6336d0153c017a8800a455dfc2b5c874c
Igor Yossugo <igor.yossugo@luizalabs.com>
2021-01-30 16:44:10 -0300

    Add generic cotation backend

--- django/currency_service/backends/cotation/backend.py
+++ django/currency_service/backends/cotation/backend.py
@@ -1,6 +1,26 @@
 from abc import abstractmethod
+from typing import List
+
+from currency_service.currency.models import Currency
+from currency_service.currency.enums import CurrencyID
 
 
 class CotationBackend:
     id = None
     name = None
+
+    @abstractmethod
+    def get(
+        self,
+        currency: Currency,
+        currency_id: CurrencyID
+    ) -> Currency:
+        pass
+
+    @abstractmethod
+    def list(
+        self,
+        currency: Currency,
+        currency_ids: List[CurrencyID] = None
+    ) -> List[Currency]:
+        pass
